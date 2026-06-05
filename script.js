9a84281b9e11815e14474fa6ebc407e67154d8b9
arya-wok <growgardenwok@gmail.com>
2026-06-06 04:04:41 +0800

    script.js

--- script.js
+++ script.js
@@ -587,10 +587,10 @@ scene.add(planet);
 
 //Ganti Deskripsi Planet
 const ringTexts = [
-  'Galaxy of love From ...',//untuk deskripsi planet layer 1
-  "I love you",//untuk deskripsi planet layer 2
-  "♡Happy Girlfriend day♡",//untuk deskripsi planet layer 3
-  "01/08/2025",//untuk deskripsi planet layer 4
+  'Galaxy of love From Arya',//untuk deskripsi planet layer 1
+  "I love you Keyodd",//untuk deskripsi planet layer 2
+  "♡Happy Birth Day♡",//untuk deskripsi planet layer 3
+  "22/06/2026",//untuk deskripsi planet layer 4
   ...(window.dataCCD && window.dataCCD.data.ringTexts ? window.dataCCD.data.ringTexts : [])
 ];
 
